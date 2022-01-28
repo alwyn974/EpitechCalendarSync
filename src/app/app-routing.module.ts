f4b948460503acf5b17ab1c75443fcd41a74c8d4
Alwyn Mattapullut <alwyn.mattapullut@epitech.eu>
2022-01-29 00:07:33 +0400

    :alembic: Starting angular project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+
+const routes: Routes = [
+  {path: "test", component: MainComponent},
+  {path: "", component: MainComponent, pathMatch: "full"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
